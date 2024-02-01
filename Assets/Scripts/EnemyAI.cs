121ed25b4a2472ebc3987c2cac053b5f6dd03c99
IDKWIAm <133805972+IDKWIAm@users.noreply.github.com>
2024-02-01 22:27:44 +0300

    Version 0.0.0.3
    Добавлен враг и его навигация.
    -----------------------------------------
    Added enemy and his navigation.
    
    Module 3.
    Theme 2.
    Lessons 1-6.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public List<Transform> patrolPoints;
+
+    private NavMeshAgent _navMeshAgent;
+    private int point;
+
+    void Start()
+    {
+        InitComponentLinks();
+        PickNewPatrolPoint();
+    }
+
+    void Update()
+    {
+        PatrolUpdate();
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        point = Random.Range(0, patrolPoints.Count);
+    }
+
+    private void PatrolUpdate()
+    {
+        if (transform.position.x == patrolPoints[point].position.x && transform.position.z == patrolPoints[point].position.z)
+        {
+            PickNewPatrolPoint();
+        }
+        _navMeshAgent.destination = patrolPoints[point].position;
+    }
+
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+}
