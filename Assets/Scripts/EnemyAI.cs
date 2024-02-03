08e3d1ab39fd085aa77fe42b88a286ef76731e23
IDKWIAm <133805972+IDKWIAm@users.noreply.github.com>
2024-02-03 20:33:11 +0300

    Version 0.0.0.4
    Добавлено взаимодействие врага с игроком.
    Внешний вид врага(и немного игрока) изменен.
    Добавлены контроллеры анимаций.
    ------------------------------------------
    Added enemy-player interaction.
    The appearance of the enemy (and a little of the player) has been changed.
    Added animation controllers.
    
    Module 3.
    Theme 2.
    Lessons 7-12.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,9 +6,12 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     public List<Transform> patrolPoints;
+    public PlayerController player;
+    public float viewAngle = 360;
 
     private NavMeshAgent _navMeshAgent;
     private int point;
+    private bool _isPlayerNoticed;
 
     void Start()
     {
@@ -18,7 +21,14 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
+        NoticePlayerUpdate();
+        ChaseUpdate();
         PatrolUpdate();
+    }
+
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     private void PickNewPatrolPoint()
@@ -26,17 +36,42 @@ public class EnemyAI : MonoBehaviour
         point = Random.Range(0, patrolPoints.Count);
     }
 
+    private void NoticePlayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+
+        _isPlayerNoticed = false;
+
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+    }
+
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+        else
+        {
+            _navMeshAgent.destination = patrolPoints[point].position;
+        }
+    }
+
     private void PatrolUpdate()
     {
-        if (transform.position.x == patrolPoints[point].position.x && transform.position.z == patrolPoints[point].position.z)
+        if (_navMeshAgent.remainingDistance <= 0)
         {
             PickNewPatrolPoint();
         }
-        _navMeshAgent.destination = patrolPoints[point].position;
-    }
-
-    private void InitComponentLinks()
-    {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 }
