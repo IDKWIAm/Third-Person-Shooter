9b9976faf073f6562e13db90a8cd2f377e0bb275
IDKWIAm <133805972+IDKWIAm@users.noreply.github.com>
2024-02-26 23:36:22 +0300

    Version 0.0.0.7
    Добавлена карта.
    ---------------------------
    Map added.

---
+++ Assets/Scripts/CameraCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    public float rayDistance;
+
+    public Transform cameraAxis;
+
+    void Update()
+    {
+        var ray = (transform.parent.position - cameraAxis.position).normalized;
+
+        RaycastHit hit;
+        if (Physics.Raycast(cameraAxis.position + new Vector3(0, 1, 0), ray, out hit, rayDistance))
+        {
+
+            transform.position = hit.point;
+        }
+        else
+        {
+            transform.position = transform.parent.position;
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterNearby.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CharacterNearby : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = gameObject.transform.parent.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            _animator?.SetBool("character_nearby", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            _animator?.SetBool("character_nearby", false);
+        }
+    }
+}
