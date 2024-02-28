ec47d864b5b92b3d01d94699ee40dc26f5bf69b3
IDKWIAm <133805972+IDKWIAm@users.noreply.github.com>
2024-02-29 00:28:01 +0300

    Version 0.0.0.8
    Карта была доработана.
    Механика аптечки добавлена.
    Экран конца игры анимирован.
    -------------------------------------------
    The map has been improved.
    Aid kit mechanics added.
    Game Over Screen Animated.
    
    Module 3.
    Theme 4.
    Lessons 1-2.

---
+++ Assets/Scripts/Aidkit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Aidkit : MonoBehaviour
+{
+    public float healAmount = 50f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+
+        if (playerHealth != null)
+        {
+            playerHealth.AddHealth(healAmount);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -8,18 +8,21 @@ public class PlayerHealth : MonoBehaviour
     public GameObject gameplayUI;
     public GameObject gameOverScreen;
 
-    private float maxHealth;
+    private float _maxHealth;
+    private bool _dead;
+    private Animator _childAnim;
 
     private void Start()
     {
-        maxHealth = health;
+        _childAnim = transform.GetChild(0).GetComponent<Animator>();
+        _maxHealth = health;
         DrawHealthBar();
     }
 
     public void DealDamage(float damage)
     {
         health -= damage;
-        if (health <= 0)
+        if (health <= 0 && !_dead)
         {
             PlayerIsDead();
         }
@@ -27,15 +30,26 @@ public class PlayerHealth : MonoBehaviour
         DrawHealthBar();
     }
 
+    public void AddHealth(float amount)
+    {
+        health += amount;
+        health = Mathf.Clamp(health, 0, _maxHealth);
+        DrawHealthBar();
+    }
+
     private void DrawHealthBar()
     {
-        valueRectTransform.localScale = new Vector2(health / maxHealth, 1);
+        valueRectTransform.localScale = new Vector2(health / _maxHealth, 1);
     }
 
     private void PlayerIsDead()
     {
         gameplayUI.SetActive(false);
         gameOverScreen.SetActive(true);
+
+        _childAnim.SetTrigger("Dead");
+        _dead = true;
+
         GetComponent<PlayerController>().enabled = false;
         GetComponent<BulletCaster>().enabled = false;
         GetComponent<CameraRotation>().enabled = false;
