18ce696c2f8bcd00bf5d86b25e5564b84c347c51
IDKWIAm <133805972+IDKWIAm@users.noreply.github.com>
2024-03-15 23:07:27 +0300

    Version 0.0.1.1
    Интерфейс здоровья игрока улучшен.
    -------------------------------------------
    Improved player health bar.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -8,16 +8,45 @@ public class PlayerHealth : MonoBehaviour
     public GameObject gameplayUI;
     public GameObject gameOverScreen;
 
+    public float lerpSpeed = 1f;
+
+    private float _lerpValue;
     private float _maxHealth;
     private bool _dead;
+    private bool _lerpIncrease;
+    private bool _lerpDecrease;
+
     private Animator _childAnim;
 
     private void Start()
     {
         _childAnim = transform.GetChild(0).GetComponent<Animator>();
         _maxHealth = health;
-        DrawHealthBar();
+        DrawHealthBar(0f);
     }
+
+    //private void Update()
+    //{
+    //    if (_lerpIncrease)
+    //    {
+    //        _lerpValue += lerpSpeed * Time.deltaTime;
+    //        
+    //        if (_lerpValue >= 1)
+   //         {
+   //             _lerpIncrease = false;
+   //         }
+   //      }
+
+   //      if (_lerpDecrease)
+   //      {
+   //          _lerpValue -= lerpSpeed * Time.deltaTime;
+
+   //         if (_lerpValue <= 0)
+   //         {
+   //             _lerpDecrease = false;
+   //         }
+   //     }
+   // }
 
     public void DealDamage(float damage)
     {
@@ -26,20 +55,33 @@ public class PlayerHealth : MonoBehaviour
         {
             PlayerIsDead();
         }
-
-        DrawHealthBar();
+        _lerpDecrease = true;
+        DrawHealthBar(damage);
     }
 
-    public void AddHealth(float amount)
+    public void AddHealth(float healAmount)
     {
-        health += amount;
+        health += healAmount;
         health = Mathf.Clamp(health, 0, _maxHealth);
-        DrawHealthBar();
+        _lerpIncrease = true;
+        DrawHealthBar(healAmount);
     }
 
-    private void DrawHealthBar()
+    private void DrawHealthBar(float changeValue)
     {
-        valueRectTransform.localScale = new Vector2(health / _maxHealth, 1);
+        //if (_lerpIncrease)
+        //{
+        //    _lerpValue = 0f;
+        //    valueRectTransform.anchorMax = Vector2.Lerp(new Vector2(health - changeValue / _maxHealth, 1), new Vector2(health / _maxHealth, 1), _lerpValue);
+        //}
+
+        //if (_lerpDecrease)
+        //{
+        //    _lerpValue = 1f;
+        //    valueRectTransform.anchorMax = Vector2.Lerp(new Vector2(health + changeValue / _maxHealth, 1), new Vector2(health / _maxHealth, 1), _lerpValue);
+        //    _lerpDecrease = false;
+        //}
+        valueRectTransform.anchorMax = new Vector2(health / _maxHealth, 1);
     }
 
     private void PlayerIsDead()
